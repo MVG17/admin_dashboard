57e83fd3474fdd8d8d1cdcaaf843547d03e4aecd
Manjunath V Gajendaragad <manjuvg7026@gmail.com>
2024-08-29 21:15:55 +0530

    project complete

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { Route, Routes } from "react-router-dom";
+
+import OverviewPage from "./pages/Overviewpage";
+import ProductsPage from "./pages/Productspage";
+import Sidebar from "./components/sidebar";
+import UsersPage from "./pages/UsersPage";
+import SalesPage from "./pages/SalesPage";
+import OrdersPage from "./pages/OrdersPage";
+import AnalyticsPage from "./pages/AnalyticsPage";
+import SettingsPage from "./pages/SettingsPage";
+
+
+
+
+function App() {
+	return (
+		<div className='flex h-screen bg-gray-900 text-gray-100 overflow-hidden'>
+			{/* BG */}
+			<div className='fixed inset-0 z-0'>
+				<div className='absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80' />
+				<div className='absolute inset-0 backdrop-blur-sm' />
+			</div>
+      <Sidebar/>
+			<Routes>
+				 <Route path="/" element={<OverviewPage/>}></Route>
+				 <Route path="/products" element={<ProductsPage/>}></Route>
+				 <Route path="/users" element={<UsersPage/>}></Route>
+				 <Route path="/sales" element={<SalesPage/>}></Route>
+				 <Route path="/orders" element={<OrdersPage/>}></Route>
+				  <Route path='/analytics' element={<AnalyticsPage />} />
+				 <Route path='/settings' element={<SettingsPage />} /> 
+			</Routes>
+		</div>
+	);
+}
+
+export default App;
